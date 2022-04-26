60b169c606eea5651c5774d24316ac5b0084d655
Mikołaj Kliche <mikolaj.kliche@unilodz.eu>
2022-04-26 14:46:11 +0200

    additional navigation option

---
+++ app/src/main/java/pl/example/fitapp/ui/recipes/RecipesViewModel.java
@@ -0,0 +1,18 @@
+package pl.example.fitapp.ui.recipes;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class RecipesViewModel extends ViewModel {
+    private MutableLiveData<String> mText;
+
+    public RecipesViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is recipes fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
